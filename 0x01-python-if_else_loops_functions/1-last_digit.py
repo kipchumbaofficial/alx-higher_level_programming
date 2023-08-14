79151aa11965a1874f2dca82df65134daa08630b
Victor Kipchumba <victorkipchumba039@gmail.com>
2023-08-14 08:50:36 +0000

    Task 1: correction

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,7 +7,7 @@ if number < 0:
 if remainder > 5:
     print('Last digit of', number, 'is', remainder, 'and is greater than 5')
 elif remainder < 6 and remainder != 0:
-    print('Last digit of', number, 'is', remainder, 
+    print('Last digit of', number, 'is', remainder,\
             'and is less than 6 and not 0')
 else:
     print('Last digit of', number, 'is', remainder, 'and is 0')
