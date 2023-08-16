3e4f1d9eabfdf33418a633d7737903c48e2bf728
Victor Kipchumba <victorkipchumba039@gmail.com>
2023-08-16 06:49:20 -0700

    Task 1: squared simple

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    squares = []
+    for i in range(len(matrix)):
+        squares.append(list(map(lambda x: x ** 2, matrix[i])))
+    return(squares)
