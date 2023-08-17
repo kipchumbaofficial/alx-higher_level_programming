1115d7816f373c964a2faf69f0fb6176712f6759
Victor Kipchumba <victorkipchumba039@gmail.com>
2023-08-16 21:21:03 -0700

    Task 1: Correction

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,4 +3,4 @@ def square_matrix_simple(matrix=[]):
     squares = []
     for i in range(len(matrix)):
         squares.append(list(map(lambda x: x ** 2, matrix[i])))
-    return(squares)
+    return squares
