b2d2a4e4c0ebe58607bf93ce6592f17814c39120
Victor Kipchumba <victorkipchumba039@gmail.com>
2023-08-16 20:01:12 -0700

    Task 12: Roman to integer Correction

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,7 @@ def roman_to_int(roman_string):
     my_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     my_int = 0
     if roman_string == None:
-        return my int
+        return my_int
     for i in roman_string:
         for key, value in my_dict.items():
             if key == i:
