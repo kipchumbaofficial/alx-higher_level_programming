fde7ab4268635ea122aced11e82cb5979a662e55
Victor Kipchumba <victorkipchumba039@gmail.com>
2023-08-19 03:54:14 -0700

    Task 5: Correction

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def no_c(my_string):
     if my_string is not None:
+        temp = my_string[:]
         for i in my_string:
             if i == 'c' or i == 'C':
-                return my_string.translate({ord(i): None})
+                temp = my_string.translate({ord(i): None})
+            my_string = temp
+        return temp
