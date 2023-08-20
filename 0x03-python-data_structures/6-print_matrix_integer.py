dfb81a056299dd6ffcba80b37009ee66191388e4
Victor Kipchumba <victorkipchumba039@gmail.com>
2023-08-20 01:36:51 -0700

    Task 6: pycodestyle

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[]):
-    for l in matrix:
-        for i, n in enumerate(l):
+    for row in matrix:
+        for i, n in enumerate(row):
             print("{:d}".format(n), end="")
-            if i < len(l) - 1:
+            if i < len(row) - 1:
                 print(" ",  end="")
         print()
