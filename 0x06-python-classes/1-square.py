a299ddfcee01a0e90334735277e70c9551e63dec
Victor Kipchumba <morale.softwaredev@gmail.com>
2023-09-17 21:16:03 -0700

    Task 1: 1. Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/in/python3
+'''
+A class definition
+'''
+
+
+class Square:
+    '''
+    Class with private instance attribute size
+    '''
+
+    def __init__(self, size):
+        self__size = size
