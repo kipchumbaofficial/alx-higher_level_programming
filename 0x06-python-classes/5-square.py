6ad74720954b0a85fe006cfef257347ca50f54cc
Victor Kipchumba <morale.softwaredev@gmail.com>
2023-09-18 15:05:08 -0700

    Task 5: my_print

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+'''
+A class with print
+'''
+
+
+class Square:
+    '''
+    square with my_print method
+    '''
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.__size
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        for i in range(self.__size):
+            for col in range(self.__size):
+                print("#", end="")
+            print()
