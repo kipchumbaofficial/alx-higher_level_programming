1a9b6ea1d827ffd769b3c9e328c66c7cc1bdb629
Victor Kipchumba <morale.softwaredev@gmail.com>
2023-09-18 07:13:33 -0700

    Task 4: Access and update private attribute

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+'''
+Class with getter and setter
+'''
+
+
+class Square:
+    '''
+    setter and getter
+    '''
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.__size ** 2
