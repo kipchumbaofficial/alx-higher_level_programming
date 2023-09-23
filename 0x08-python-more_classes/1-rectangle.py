a2577913b4cca5f96f27bad2ac1a1aeb6ec33f2f
Victor Kipchumba <morale.softwaredev@gmail.com>
2023-09-22 18:50:45 -0700

    Task 1: exceptions

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -10,8 +10,16 @@ class Rectangle:
     '''
 
     def __init__(self, width=0, height=0):
+        if type(width) != int:
+            raise TypeError("width must be an integer")
+        elif width < 0:
+            raise ValueError("width must be >= 0")
         self.__width = width
-        self.__height = height
+        if type(height) != int:
+            raise TypeError("height must be an integer")
+        elif height < 0:
+            raise ValueError("height must be >= 0")
+        self.__heigth = height
 
     @property
     def height(self):
