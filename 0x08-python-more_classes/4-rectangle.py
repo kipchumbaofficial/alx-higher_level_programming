6ba7d6a9b0457ba11990b88c304bb4d4d72a2a90
victor kipchumba <victorkipchumba039@gmail.com>
2023-12-10 05:05:27 +0300

    Task 4: Eval is magic

---
+++ 0x08-python-more_classes/4-rectangle.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python3
+'''
+A real class
+'''
+
+
+class Rectangle:
+    '''
+    A rectangle with width and height
+    Has perimeter and area methods
+    '''
+
+    def __init__(self, width=0, height=0):
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = width
+
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return ((self.__width * 2) + (self.__height * 2))
+
+    def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        retval = ""
+        for i in range(self.__height):
+            for j in range(self.__width):
+                retval += "#"
+            if i < (self.__height - 1):
+                retval += "\n"
+        return retval
+
+    def __repr__(self):
+        str_rep = "Rectangle(" + str(self.__width)
+        str_rep += "," + str(self.__height) + ")"
+        return str_rep
