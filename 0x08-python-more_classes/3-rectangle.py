bea513a7a3999aeab017ec3106f6132cb1c29261
victor kipchumba <victorkipchumba039@gmail.com>
2023-12-10 04:39:29 +0300

    Task 3: String representation correction

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -63,5 +63,6 @@ class Rectangle:
         for i in range(self.__height):
             for j in range(self.__width):
                 retval += "#"
-            retval += "\n"
+            if i < (self.__height - 1):
+                retval += "\n"
         return retval
