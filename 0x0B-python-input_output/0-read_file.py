064f587f453371a402c5324596242080ad3dfbd2
victor kipchumba <victorkipchumba039@gmail.com>
2023-12-28 11:16:03 +0300

    Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -10,5 +10,4 @@ def read_file(filename=""):
     Reads file
     '''
     with open(filename, "r", encoding="utf-8") as f:
-        read_data = f.read()
-    print(read_data)
+        print(f.read())
