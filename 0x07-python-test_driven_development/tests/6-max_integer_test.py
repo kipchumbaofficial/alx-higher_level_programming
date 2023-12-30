be7f722fb036bb743c70781c85bcc794e0ae5a10
victor kipchumba <victorkipchumba039@gmail.com>
2023-12-30 19:58:21 +0300

    Task 5: Correction

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -14,6 +14,11 @@ class TestMaxInteger(unittest.TestCase):
         self.test_list = [1, 5, 4, 2, 9, 3]
         self.empty_list = []
         self.none_error = "object of type 'NoneType' has no len()"
+        self.max_beginning = [5, 3, 2, 4]
+        self.max_ending = [5, 3, 4, 7, 10]
+        self.test_negatives = [-4, -10, -3, -6, -8]
+        self.one_negative = [1, 3, 6, -4, 10]
+        self.one_element = [10]
 
     """Test if list is empty"""
     def test_empty(self):
@@ -32,6 +37,22 @@ class TestMaxInteger(unittest.TestCase):
         with self.assertRaises(TypeError) as err:
             c = max_integer(None)
         self.assertEqual(str(err.exception), self.none_error)
+
+    """Test max in the beginning"""
+    def test_max_start(self):
+        self.assertEqual(max_integer(self.max_beginning), 5)
+
+    def test_max_end(self):
+        self.assertEqual(max_integer(self.max_ending), 10)
+
+    def test_negatives(self):
+        self.assertEqual(max_integer(self.test_negatives), -3)
+
+    def test_one_negative(self):
+        self.assertEqual(max_integer(self.one_negative), 10)
+
+    def test_one_element(self):
+        self.assertEqual(max_integer(self.one_element), 10)
 
 
 if __name__ == '__main__':
