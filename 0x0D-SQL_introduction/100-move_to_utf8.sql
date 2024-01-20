b45555dd8cc4aad555b93708e5a488230b6d7564
victor kipchumba <victorkipchumba039@gmail.com>
2024-01-20 07:47:38 +0300

    Task 17: go to UTF8

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,16 @@
+-- Convert a database to utf8
+-- collate utf8mb4_unicode_ci
+USE hbtn_0c_0;
+
+ALTER DATABASE hbtn_0c_0
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
+
+ALTER TABLE first_table
+CONVERT TO CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
+
+ALTER TABLE first_table
+MODIFY name VARCHAR(255)
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
