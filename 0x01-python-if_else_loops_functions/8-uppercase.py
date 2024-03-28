e9823a4b65c54ddea234fd7845bae7bf6bcbbc1e
Victor Kipchumba <victorkipchumba039@gmail.com>
2024-03-28 22:46:26 +0300

    Task 8: Corrrection

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,13 +1,6 @@
 #!/usr/bin/python3
 
-'''
-Print lowercase to upper
-'''
-
-
 def uppercase(str):
-    '''Function printing lowercase to uppercase
-    '''
     for char in str:
         if ord(char) in range(97, 123):
             new_asci = ord(char) - 32
