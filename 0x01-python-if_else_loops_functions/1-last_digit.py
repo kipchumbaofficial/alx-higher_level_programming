b63db9bc47e1e654f6b7c1222a4a3a3b50d1eaa7
Victor Kipchumba <victorkipchumba039@gmail.com>
2024-03-28 19:28:58 +0300

    Task 1: pycodestyle correction

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,6 +8,6 @@ if remainder > 5:
     print('Last digit of', number, 'is', remainder, 'and is greater than 5')
 elif remainder < 6 and remainder != 0:
     print('Last digit of', number, 'is', remainder,
-            'and is less than 6 and not 0')
+          'and is less than 6 and not 0')
 else:
     print('Last digit of', number, 'is', remainder, 'and is 0')
