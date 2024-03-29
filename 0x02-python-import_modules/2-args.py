5710215968e60265448d2225ca665c49315e5dad
Victor Kipchumba <victorkipchumba039@gmail.com>
2024-03-29 20:58:51 +0300

    TAsk 2: How to make code dynamic

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+from sys import argv
+
+length = len(argv) - 1
+
+if length == 0:
+    print("{} arguments.".format(length))
+elif length == 1:
+    print("{} argument:".format(length))
+else:
+    print("{} arguments:".format(length))
+
+for index in range(length):
+    print("{}: {}".format(index + 1, argv[index + 1]))
