d2a7195f937f1c95d91328958e0cd8aec0c86d6c
Victor Kipchumba <victorkipchumba039@gmail.com>
2024-03-29 14:05:56 +0300

    TAsk 1: My first toolbox

---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+def add(a, b):
+    """My addition function
+    Args:
+        a: first integer
+        b: second integer
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
+
+def sub(a, b):
+    """MY subtraction function
+    Args:
+        a: first integer
+        b: second integer
+    Returns:
+        THe return value. a - b
+    """
+    return (a -b)
+
+def mul(a, b):
+    """My multiplication function
+
+    Args:
+        a: first integer
+        b: second integer
+    Returns:
+        The return value a * b
+    """
+    return (a * b)
+
+def div(a, b):
+    """My division function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value a / b
+    """
+    return int(a / b)
