f234da9cbb1a3c9957fe25052bef3414b25f25e5
Victor Kipchumba <victorkipchumba039@gmail.com>
2024-03-29 21:16:32 +0300

    Task 2: Correction

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,17 +2,18 @@
 
 from sys import argv
 
-length = len(argv) - 1
+def main():
+    length = len(argv) - 1
 
-if length == 0:
-    print("{} arguments.".format(length))
-elif length == 1:
-    print("{} argument:".format(length))
-else:
-    print("{} arguments:".format(length))
+    if length == 0:
+        print("{} arguments.".format(length))
+    elif length == 1:
+        print("{} argument:".format(length))
+    else:
+        print("{} arguments:".format(length))
 
-for index in range(length):
-    print("{}: {}".format(index + 1, argv[index + 1]))
+    for index in range(length):
+        print("{}: {}".format(index + 1, argv[index + 1]))
 
 if __name__ == "__main__":
     main()
