722c94b6aacc552d5fa412bac52f8f46d5fdee53
Victor Kipchumba <victorkipchumba039@gmail.com>
2024-03-29 13:35:15 +0300

    Task 0: Correction

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -5,7 +5,7 @@ from add_0 import add
 a = 1
 b = 2
 
-print("{} + {} = {}".format(a, b, add(a, b)))
+print("{} + {} = {}".format(a, b, add(int(a), int(b))))
 
 if __name__ == "__main__":
-    add()
+    add(a, b)
