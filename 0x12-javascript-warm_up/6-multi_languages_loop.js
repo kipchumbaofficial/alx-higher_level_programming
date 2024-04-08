a02f9ae68b36737772456f645402e6abcbeb253b
Victor Kipchumba <victorkipchumba039@gmail.com>
2024-04-08 22:00:30 +0300

    Task 6. Loop to languages

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+// Multi language
+
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (const elements of languages) {
+  console.log(elements);
+}
