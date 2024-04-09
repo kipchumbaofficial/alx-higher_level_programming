927b12baded79c893fae7b453049d25b9532d564
Victor Kipchumba <victorkipchumba039@gmail.com>
2024-04-09 15:58:16 +0300

    Task 0: Rectangle

---
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+// Empty class Rectangle
+
+class Rectangle {}
+
+module.exports = Rectangle
---
+++ 0x13-javascript_objects_scopes_closures/README.md
@@ -0,0 +1,3 @@
+0x13-javascript_objects_scopes_closures
+---
+OOJS, Objects, Scope & Closures
