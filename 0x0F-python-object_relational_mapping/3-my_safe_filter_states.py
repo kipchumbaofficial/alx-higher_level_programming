24e1c130e4f07eb1a78c50a00f675df911332203
Victor Kipchumba <victorkipchumba039@gmail.com>
2024-04-13 13:24:46 +0300

    Task 3: SQL Injection...

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -5,8 +5,24 @@
 import sys
 import MySQLdb
 
-def main():
-    if len(sys.argv) != 5:
-        sys.exit(1)
 
-    user = sys.arg
+def main():
+    user = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    searched = sys.argv[4]
+
+    db = MySQLdb.connect(host='localhost', port=3306, user=user,
+                         passwd=password, db=database)
+    cur = db.cursor()
+    searched = searched.replace("'", "''")
+    query = "SELECT * FROM states WHERE name "
+    query += "LIKE BINARY '%{}%' ORDER BY id".format(searched)
+    cur.execute(query)
+    rows = cur.fetchall()
+    for row in rows:
+        print(f"({row[0]}, '{row[1]}')")
+
+
+if __name__ == '__main__':
+    main()
