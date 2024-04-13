3ce77890d231327c0c56a977da854916460a4aa9
Victor Kipchumba <victorkipchumba039@gmail.com>
2024-04-13 06:46:38 +0300

    Task 1: Filter states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''A Script to filter and print out states
+Starting with letter N from a database
+'''
+import MySQLdb
+import sys
+
+
+def main():
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    db = MySQLdb.connect(host='localhost', port=3306, user=username,
+                         passwd=password, db=database)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+    rows = cur.fetchall()
+    for row in rows:
+        print(f"({row[0]}, '{row[1]}')")
+
+
+if __name__ == '__main__':
+    main()
