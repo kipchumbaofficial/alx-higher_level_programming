8744ccc92894df817c07b0c5ffaa71f245a66816
Victor Kipchumba <victorkipchumba039@gmail.com>
2024-04-13 05:50:39 +0300

    Task 0: Get all states

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+'''A module to select all states from a database
+and print out the values
+'''
+import sys
+import MySQLdb
+user = sys.argv[1]
+password = sys.argv[2]
+database = sys.argv[3]
+db = MySQLdb.connect(host='localhost', port=3306, user=user,
+                     passwd=password, db=database)
+cur = db.cursor()
+cur.execute("SELECT * FROM states ORDER BY id")
+
+
+if __name__ == '__main__':
+    rows = cur.fetchall()
+    for row in rows:
+        print(f"({row[0]}, {row[1]})")
