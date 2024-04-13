a9af906c7f3b040dd1c3149ef6227baa785c6400
Victor Kipchumba <victorkipchumba039@gmail.com>
2024-04-13 07:48:28 +0300

    Task 2: Filter states by user input

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+'''
+A script that take an argument
+Selects from database based on the argument
+'''
+import sys
+import MySQLdb
+
+
+def main():
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    searched = sys.argv[4]
+
+    db = MySQLdb.connect(host='localhost', port=3306, user=username,
+                         passwd=password, db=database)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name\
+                 LIKE {} ORDER BY id".format("'" + searched + "'"))
+    rows = cur.fetchall()
+    for row in rows:
+        print(f"({row[0]}, '{row[1]}')")
+
+
+if __name__ == '__main__':
+    main()
