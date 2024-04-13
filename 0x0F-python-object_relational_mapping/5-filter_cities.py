9e07c5fe4c1cd16c88bb2a693ae6d9bd578e5ac6
Victor Kipchumba <victorkipchumba039@gmail.com>
2024-04-13 18:28:16 +0300

    Task 5: 5. All cities by state

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+'''
+Takes in name of state
+Spits all the cities in that state
+'''
+import MySQLdb
+from sys import argv
+
+
+def main():
+    user = argv[1]
+    password = argv[2]
+    database = argv[3]
+    state = argv[4]
+
+    db = MySQLdb.connect(host='localhost', port=3306, user=user,
+                         passwd=password, db=database)
+    cur = db.cursor()
+    state = state.replace("'", "''")
+    query = "SELECT cities.* FROM cities "
+    query += "JOIN states ON cities.state_id=states.id "
+    query += "WHERE states.name LIKE '%{}%'".format(state)
+
+    cur.execute(query)
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row[2], end=", ")
+    print()
+
+
+if __name__ == '__main__':
+    main()
