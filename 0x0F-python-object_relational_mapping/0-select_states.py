b7c74e9beaca29d212c2e4a68255a3ab0ef09010
Victor Kipchumba <victorkipchumba039@gmail.com>
2024-04-13 05:58:55 +0300

    Task 0: More correction

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,16 +5,17 @@ and print out the values
 '''
 import sys
 import MySQLdb
-user = sys.argv[1]
-password = sys.argv[2]
-database = sys.argv[3]
-db = MySQLdb.connect(host='localhost', port=3306, user=user,
-                     passwd=password, db=database)
-cur = db.cursor()
-cur.execute("SELECT * FROM states ORDER BY id")
-
-
-if __name__ == '__main__':
+def main():
+    '''Function to execute the procces'''
+    user = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    db = MySQLdb.connect(host='localhost', port=3306, user=user,
+                         passwd=password, db=database)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id")
     rows = cur.fetchall()
     for row in rows:
         print(f"({row[0]}, '{row[1]}')")
+if __name__ == '__main__':
+    main()
