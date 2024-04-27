5b06c7c562e8a742e2baa5729b8b6527c1ccd2c8
Victor Kipchumba <victorkipchumba039@gmail.com>
2024-04-27 18:16:24 +0300

    Task 9: Correction

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -7,12 +7,14 @@ Uses github API to display your id
 if __name__ == "__main__":
     from sys import argv
     import requests
+    from requests.auth import HTTPBasicAuth
 
     username = argv[1]
     token = argv[2]
     url = 'https://api.github.com/user'
-    headers = {'Authorization': f'Bearer {token}'}
-    response = requests.post(url, headers=headers, auth=(username, token))
+    auth = HTTPBasicAuth(username, token)
+
+    response = requests.post(url, auth=auth)
     json_response = response.json()
 
     if json_response:
