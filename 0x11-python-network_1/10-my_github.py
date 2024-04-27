8669a53a5f93884f5be6e1c025a023b91ea96255
Victor Kipchumba <victorkipchumba039@gmail.com>
2024-04-27 14:30:28 +0300

    Task 9: my github!

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Takes in Github Credentilas
+Uses github API to display your id
+"""
+
+
+if __name__ == "__main__":
+    from sys import argv
+    import requests
+
+    username = argv[1]
+    password = argv[2]
+    url = 'https://api.github.com/user'
+
+    response = requests.post(url, auth=(username, password))
+    json_response = response.json()
+
+    if json_response:
+        identity = json_response.get('id')
+        print(identity)
