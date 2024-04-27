15f95c329a94cafee4b98a79c74a6208060c9ec2
Victor Kipchumba <victorkipchumba039@gmail.com>
2024-04-27 12:40:10 +0300

    Task 8:  Search API

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Takes in a letter
+sends a POST requests with letter as parameter
+"""
+
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+
+
+    payload = {'q': ""}
+    if argv[1]:
+        payload[q] = argv[1]
+    url = 'http://0.0.0.0:5000/search_user'
+    response = requests.post(url, data=payload)
+
+    if 'application/json' in response.headers.get('Content-Type', ''):
+        json_response = response.json()
+        if not json_response:
+            print("No result")
+        else:
+            identity = json_response.get('id')
+            name = json_response.get('name')
+            print(f"{[id]} {name}")
+    else:
+        print("Not a valid JSON")
