61b47f5e3807b36f5c9a65be782ac4a1b42549d5
Victor Kipchumba <victorkipchumba039@gmail.com>
2024-04-30 09:38:59 +0300

    Task 10. Time for an interview!

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -3,21 +3,13 @@
 prints out sha and author of commit
 """
 
-
 if __name__ == "__main__":
     from sys import argv
     import requests
-
-    repo = argv[1]
-    owner = argv[2]
-    url = f'https://api.github.com/repos/{owner}/{repo}/commits'
+    url = f'https://api.github.com/repos/{argv[1]}/{argv[2]}/commits'
     response = requests.get(url)
 
-    json_response = response.json()
-    info = json_response[-10:]
+    info = response.json()[-10:]
     for commit in reversed(info):
-        sha = commit.get('sha')
-        commit = commit.get('commit')
-        author = commit.get('author')
-        name = author.get('name')
-        print(f"{sha} {name}")
+        name = commit.get('commit').get('author').get('name')
+        print(f"{commit.get('sha')}: {name}")
