516f04dc971bb2a908acd7cb359baf531c08433f
Victor Kipchumba <victorkipchumba039@gmail.com>
2024-05-22 19:18:08 +0300

    Task 0: Readme

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// Reads and print
+
+const path = process.argv[2];
+const fs = require('fs');
+
+fs.readFile(path, 'utf-8', (error, data) => {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log(data);
+    }
+});
