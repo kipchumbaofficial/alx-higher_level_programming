8d5041f91d61dc65a6aa77f77c23d527f452117e
Victor Kipchumba <victorkipchumba039@gmail.com>
2024-05-23 08:14:14 +0300

    Task 2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -8,6 +8,6 @@ request(url, function (error, response, body) {
     if (error) {
         console.log(error);
     } else {
-        console.log('code:', reponse && response.statusCode);
+        console.log('code:', response && response.statusCode);
     }
 });
