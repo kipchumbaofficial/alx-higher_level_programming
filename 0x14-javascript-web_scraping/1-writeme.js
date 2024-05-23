d863713f71b7209423142efc76cfbee65fd26b1e
Victor Kipchumba <victorkipchumba039@gmail.com>
2024-05-23 07:34:45 +0300

    Task 1: Correction

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -6,7 +6,7 @@ const path = process.argv[2];
 const data = `${process.argv[3]}\n`;
 
 fs.writeFile(path, data, 'utf-8', (err) => {
-    if (err) {
-        console.log(err);
-    }
+  if (err) {
+    console.log(err);
+  }
 });
