a6146c51efd51657bdab00940e5db789d6e8b6a8
Victor Kipchumba <victorkipchumba039@gmail.com>
2024-05-23 08:19:58 +0300

    Task 2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,12 +2,12 @@
 // Get status code of a request
 
 const url = process.argv[2];
-let request = require('request');
+const request = require('request');
 
 request(url, function (error, response, body) {
-    if (error) {
-        console.log(error);
-    } else {
-        console.log('code:', response && response.statusCode);
-    }
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('code:', response && response.statusCode);
+  }
 });
