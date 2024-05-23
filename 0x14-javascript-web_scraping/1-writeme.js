be3a6b746b86cd0f3ce5724d1563c87d865d2dc8
Victor Kipchumba <victorkipchumba039@gmail.com>
2024-05-23 07:36:31 +0300

    Task 1: more correction

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,7 +3,7 @@
 
 const fs = require('fs');
 const path = process.argv[2];
-const data = `${process.argv[3]}\n`;
+const data = process.argv[3];
 
 fs.writeFile(path, data, 'utf-8', (err) => {
   if (err) {
